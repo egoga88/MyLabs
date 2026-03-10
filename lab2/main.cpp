8b820fd93711b755d71d91db4147fd1803f0535c
Egoga <tnata@gmail.com>
2026-03-10 11:45:41 +0400

    0.1

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -3,6 +3,15 @@ import LabUtils;
 using namespace std;
 
 
+void(vector<int>& vec, int n) {
+    for (int i = 0; i < n; i++) {
+        cin << vector()
+    }
+}
+
+
+
+
 int main(){
     printMetadata(2, "Аббасов Теймур");
     print("Введите кол-во элементов последовательности: ");
@@ -14,10 +23,13 @@ int main(){
     vector<int>(n, 0);
     printSeparLine(22);
     println("Выберите способ заполнения: \n[1] - Вручную. \n[2] - Случайным образом.");
-    print(">");
+    print("> ");
     int fillingMethod = 1;
     cin >> fillingMethod;
     printSeparLine(22);
+    if (fillingMethod == 1) {
+        for (int i)
+    }
 
 
 
